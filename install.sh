ae3129419215f2f31ee446cfa0ca22a733db28fd
Silvio Ramalho Neto da Silva <silvioramalhoneto18@outlook.com.br>
2026-02-04 21:05:50 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p ~/.fonts/
+
+cp fonts/NovaMono-for-Powerline.ttf ~/.fonts/
+
+cp -r icewm/themes/Icewm_Nord_style/ ~/.icewm/themes/
+
+cp -r icewm/themes/Icewm_Nord_style_small/ ~/.icewm/themes/
+
+cp -r icewm/themes/Icewm_Nord_style_Light/ ~/.icewm/themes/
+
+echo $"Dotfiles Instalado Com Sucesso"
